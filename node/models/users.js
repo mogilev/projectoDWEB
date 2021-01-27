316471443ee0b3861480bcb79eaf0722ca5e4e58
Koja_Mutilator <artur.jpfernandes@gmail.com>
2021-01-27 21:21:11 +0000

    users

--- node/models/users.js
+++ node/models/users.js
@@ -1,5 +1,4 @@
 module.exports = (sequelize, type) =>
-<<<<<<< HEAD
     sequelize.define('users', {
         id: {
             type: type.INTEGER,
@@ -24,31 +23,3 @@ module.exports = (sequelize, type) =>
         resetPasswordToken: type.STRING,
         resetPasswordExpires: type.DATE,
     });
-
-/* Não sei se este está certo */
-=======
-  sequelize.define('users', {
-    id: {
-      type: type.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
-    isAdmin: type.boolean,
-    first_name: type.STRING,
-    last_name: type.STRING,
-    email: {
-      type: type.STRING,
-      allowNull: false,
-    },
-    username: {
-      type: type.STRING,
-      allowNull: false,
-    },
-    password: {
-      type: type.STRING,
-      allowNull: false,
-    },
-    resetPasswordToken: type.STRING,
-    resetPasswordExpires: type.DATE,
-  });
->>>>>>> 73071d4d2889a1a8ee1f5ffaee14925cbfa4fd78
