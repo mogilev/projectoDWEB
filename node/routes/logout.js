bde0d0d8e354a548b7dab0cc55d50515305efa4c
mogilev <58267935+mogilev@users.noreply.github.com>
2021-01-27 22:30:54 +0000

    added routes no node antigo

---
+++ node/routes/logout.js
@@ -0,0 +1,10 @@
+module.exports = (app) => {
+    app.post("/logout", (req, res, next) => {
+              res.status(200).send({
+                auth: false,
+                message: "Logout!",
+              });
+            });
+    }
+
+//  copiado
